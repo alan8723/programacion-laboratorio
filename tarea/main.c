9ddfdbd3a8fe0e3985af7ad518ba78d0a425e8c7
alan8723 <alanixwrd@gmail.com>
2019-09-17 14:05:48 -0300

    actualizacion 17/09/19

---
+++ tarea/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 5
+
+
+int main()
+{
+    int legajo[TAM]={936, 458, 123, 448, };
+    int edad[TAM]={21, 25, 27, 20,};
+    char sexo[TAM]={'f', 'm', 'f', 'f', };
+    int n1[TAM]={8, 5, 9, 10, 10};
+    int n2[TAM]={7, 5, 6, 3, 9};
+    float promedio[TAM] = {6.33, 5.33, 8, 6.66, };
+
+    printf("Hello world!\n");
+    return 0;
+}
